e71c5e15d4f66f73aa0cf60ed3b7a771cc78673d
radness <yh.shin@miracom.co.kr>
2022-09-15 14:25:08 +0900

    gitignore setting

---
+++ pro/src/rowCutting/RowCuttingMain.java
@@ -0,0 +1,5 @@
+package rowCutting;
+
+public class RowCuttingMain {
+
+}
---
+++ pro/src/rowCutting/Sample.java
@@ -0,0 +1,35 @@
+package rowCutting;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Sample {
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			System.out.println(Integer.parseInt(st.nextToken()));
+		}
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("a");
+		sb.append("b");
+		sb.append("c");
+		sb.delete(0, 1);
+
+//		System.out.println(sb.toString());
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(sb.toString());
+//		bw.flush();
+		bw.close();
+	}
+}
