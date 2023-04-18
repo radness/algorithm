0ecf8ccc48c9cdc2b2f4b7dfed9cfd3813ff56f2
radness <yhs5128@gmail.com>
2023-04-19 08:51:57 +0900

    자릿수별 합 추가 - sum 파라미터 추가

--- pro/src/basic/recursive/RecursiveExam.java
+++ pro/src/basic/recursive/RecursiveExam.java
@@ -4,22 +4,22 @@ public class RecursiveExam {
 	static int[] path = new int[3];
 
 	public static void main(String[] args) {
-		run(0);
+		run(0, 0);
 	}
 
-	static void run(int num) {
+	static void run(int num, int sum) {
 		if (num == 3) {
 			for (int i = 0; i < num; i++) {
 				System.out.print(path[i]);
 			}
-			System.out.println();
+			System.out.println(" sum : " + sum);
 			return;
 		}
 
 		for (int i = 0; i < 3; i++) {
 			path[num] = i + 1;
 
-			run(num + 1);
+			run(num + 1, sum + i);
 			
 			path[num] = 0;
 		}
