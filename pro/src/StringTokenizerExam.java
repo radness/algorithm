bb869b0d02bbe239a4aed940c8386fda5bc63c2d
radness <yhs5128@gmail.com>
2023-04-18 10:10:36 +0900

    StringTokenizer 수정

--- pro/src/StringTokenizerExam.java
+++ pro/src/StringTokenizerExam.java
@@ -2,18 +2,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class StringTockenizer {
+public class StringTokenizerExam {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		int sum = 0;
-		
+
 		while (st.hasMoreTokens()) {
 			sum += Integer.valueOf(st.nextToken());
 		}
-		
+
 		System.out.println(sum);
 	}
 }
