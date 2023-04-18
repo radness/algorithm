e43aed9cc34b21da331660eb7b749c3739eec9e3
radness <yhs5128@gmail.com>
2023-04-18 10:11:04 +0900

    백준 1753 - 주석 수정

--- pro/src/BJ_1753.java
+++ pro/src/BJ_1753.java
@@ -1,5 +1,3 @@
-package pro.src;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -12,13 +10,13 @@ import java.util.PriorityQueue;
 
 public class BJ_1753 {
 
-    // INF 값 초기화
+    // INF 媛� 珥덇린�솕
     static final int INF = 9999999;
-    // 그래프를 표현 할 2차원 List
+    // 洹몃옒�봽瑜� �몴�쁽 �븷 2李⑥썝 List
     static List<List<Node>> graph = new ArrayList<>();
-    // 최단거리테이블을 표현 할 배열
+    // 理쒕떒嫄곕━�뀒�씠釉붿쓣 �몴�쁽 �븷 諛곗뿴
     static int[] result;
-    // 방문처리를 위한 배열이지만 저는 다른 방법으로 방문처리를 진행하겠습니다.
+    // 諛⑸Ц泥섎━瑜� �쐞�븳 諛곗뿴�씠吏�留� ���뒗 �떎瑜� 諛⑸쾿�쑝濡� 諛⑸Ц泥섎━瑜� 吏꾪뻾�븯寃좎뒿�땲�떎.
     //	static boolean[] vistied;
 
     public static void main(String[] args) throws IOException {
@@ -29,28 +27,28 @@ public class BJ_1753 {
         String[] info = br.readLine().split(" ");
         int startIndex = Integer.parseInt(br.readLine());
 
-        // 그래프 생성
+        // 洹몃옒�봽 �깮�꽦
         for (int i = 0; i < Integer.parseInt(info[0])+1; i++) {
             graph.add(new ArrayList<>());
         }
-        // 최단거리테이블 생성
+        // 理쒕떒嫄곕━�뀒�씠釉� �깮�꽦
         result = new int[Integer.parseInt(info[0])+1];
-        // 최단거리테이블 INF로 초기화
+        // 理쒕떒嫄곕━�뀒�씠釉� INF濡� 珥덇린�솕
         Arrays.fill(result, INF);
 
-        // 방문처리를 위한 배열 생성 (저는 사용하지 않습니다)
+        // 諛⑸Ц泥섎━瑜� �쐞�븳 諛곗뿴 �깮�꽦 (���뒗 �궗�슜�븯吏� �븡�뒿�땲�떎)
         // vistied = new boolean[Integer.parseInt(info[0])+1];
 
-        // 문제에서 주어진 입력 값에 따라 그래프 초기화
+        // 臾몄젣�뿉�꽌 二쇱뼱吏� �엯�젰 媛믪뿉 �뵲�씪 洹몃옒�봽 珥덇린�솕
         for (int i = 0; i < Integer.parseInt(info[1]); i++) {
             String[] temp = br.readLine().split(" ");
             graph.get(Integer.parseInt(temp[0])).add(new Node(Integer.parseInt(temp[1]), Integer.parseInt(temp[2])));
         }
 
-        // 문제에서 주어진 입력값을 바탕으로 다익스트라 알고리즘 수행
+        // 臾몄젣�뿉�꽌 二쇱뼱吏� �엯�젰媛믪쓣 諛뷀깢�쑝濡� �떎�씡�뒪�듃�씪 �븣怨좊━利� �닔�뻾
         dijkstra(startIndex);
 
-        // 문제에서 제시한 조건에 맞게 출력
+        // 臾몄젣�뿉�꽌 �젣�떆�븳 議곌굔�뿉 留욊쾶 異쒕젰
         for (int i = 1; i < result.length; i++) {
             if(result[i] == INF) {
                 bw.write("INF");
@@ -64,63 +62,63 @@ public class BJ_1753 {
 
     }
 
-    // 다익스트라 알고리즘
+    // �떎�씡�뒪�듃�씪 �븣怨좊━利�
     static void dijkstra(int index) {
 
-        // 최단거리가 갱신 된 노드들을 담을 우선순위 큐 생성
+        // 理쒕떒嫄곕━媛� 媛깆떊 �맂 �끂�뱶�뱾�쓣 �떞�쓣 �슦�꽑�닚�쐞 �걧 �깮�꽦
         PriorityQueue<Node> pq =  new PriorityQueue<>();
-        // 최단거리테이블의 시작지점노드 값 0으로 갱신
+        // 理쒕떒嫄곕━�뀒�씠釉붿쓽 �떆�옉吏��젏�끂�뱶 媛� 0�쑝濡� 媛깆떊
         result[index] = 0;
-        // 우선순위 큐에 시작노드 넣기
+        // �슦�꽑�닚�쐞 �걧�뿉 �떆�옉�끂�뱶 �꽔湲�
         pq.offer(new Node(index, 0));
 
-        // 우선순위 큐에 노드가 존재하면 계속 반복
+        // �슦�꽑�닚�쐞 �걧�뿉 �끂�뱶媛� 議댁옱�븯硫� 怨꾩냽 諛섎났
         while(!pq.isEmpty()) {
-            // 큐에서 노드 꺼내기
+            // �걧�뿉�꽌 �끂�뱶 爰쇰궡湲�
             Node node = pq.poll();
-            // 꺼낸 노드의 인덱스 및 최단거리 비용 확인
+            // 爰쇰궦 �끂�뱶�쓽 �씤�뜳�뒪 諛� 理쒕떒嫄곕━ 鍮꾩슜 �솗�씤
             int nodeIndex = node.index;
             int distance = node.distacne;
 
-            // 앞에서 주석처리했던 방문처리 배열을 사용해서 아래와 같이 방문처리하셔도 됩니다.
+            // �븵�뿉�꽌 二쇱꽍泥섎━�뻽�뜕 諛⑸Ц泥섎━ 諛곗뿴�쓣 �궗�슜�빐�꽌 �븘�옒�� 媛숈씠 諛⑸Ц泥섎━�븯�뀛�룄 �맗�땲�떎.
 //			if(vistied[nodeIndex]) {
 //				continue;
 //			}else{
 //				vistied[nodeIndex] = true;
 //			}
 
-            // 큐에서 꺼낸 거리와 최단거리테이블의 값을 비교해서 방문처리를 합니다.
-            // 큐는 최단거리를 기준으로 오름차순 정렬되고 있습니다.
-            // 만약 현재 꺼낸 노드의 거리가 최단거리테이블의 값보다 크다면 해당 노드는 이전에 방문된 노드입니다.
-            // 그러므로 해당노드와 연결 된 노드를 탐색하지 않고 큐에서 다음 노드를 꺼냅니다.
+            // �걧�뿉�꽌 爰쇰궦 嫄곕━�� 理쒕떒嫄곕━�뀒�씠釉붿쓽 媛믪쓣 鍮꾧탳�빐�꽌 諛⑸Ц泥섎━瑜� �빀�땲�떎.
+            // �걧�뒗 理쒕떒嫄곕━瑜� 湲곗��쑝濡� �삤由꾩감�닚 �젙�젹�릺怨� �엳�뒿�땲�떎.
+            // 留뚯빟 �쁽�옱 爰쇰궦 �끂�뱶�쓽 嫄곕━媛� 理쒕떒嫄곕━�뀒�씠釉붿쓽 媛믩낫�떎 �겕�떎硫� �빐�떦 �끂�뱶�뒗 �씠�쟾�뿉 諛⑸Ц�맂 �끂�뱶�엯�땲�떎.
+            // 洹몃윭誘�濡� �빐�떦�끂�뱶�� �뿰寃� �맂 �끂�뱶瑜� �깘�깋�븯吏� �븡怨� �걧�뿉�꽌 �떎�쓬 �끂�뱶瑜� 爰쇰깄�땲�떎.
             if(distance > result[nodeIndex]) {
                 continue;
             }
 
-            // 큐에서 꺼낸 노드에서 이동 가능 한 노드들을 탐색합니다.
+            // �걧�뿉�꽌 爰쇰궦 �끂�뱶�뿉�꽌 �씠�룞 媛��뒫 �븳 �끂�뱶�뱾�쓣 �깘�깋�빀�땲�떎.
             for (Node linkedNode : graph.get(nodeIndex)) {
-                // 해당노드를 거쳐서 다음 노드로 이동 할 떄의 값이 다음 이동노드의 최단거리테이블 값보다 작을 때
+                // �빐�떦�끂�뱶瑜� 嫄곗퀜�꽌 �떎�쓬 �끂�뱶濡� �씠�룞 �븷 �뻹�쓽 媛믪씠 �떎�쓬 �씠�룞�끂�뱶�쓽 理쒕떒嫄곕━�뀒�씠釉� 媛믩낫�떎 �옉�쓣 �븣
                 if(distance + linkedNode.distacne < result[linkedNode.index]) {
-                    // if문의 조건을 만족했다면 최단거리테이블의 값을 갱신합니다.
+                    // if臾몄쓽 議곌굔�쓣 留뚯”�뻽�떎硫� 理쒕떒嫄곕━�뀒�씠釉붿쓽 媛믪쓣 媛깆떊�빀�땲�떎.
                     result[linkedNode.index] = distance + linkedNode.distacne;
-                    // 갱신 된 노드를 우선순위 큐에 넣어줍니다.
+                    // 媛깆떊 �맂 �끂�뱶瑜� �슦�꽑�닚�쐞 �걧�뿉 �꽔�뼱以띾땲�떎.
                     pq.offer(new Node(linkedNode.index, result[linkedNode.index]));
                 }
             }
         }
     }
 
-    // 우선순위 큐에서 정렬기준을 잡기위해 Comparable를 구현합니다.
+    // �슦�꽑�닚�쐞 �걧�뿉�꽌 �젙�젹湲곗��쓣 �옟湲곗쐞�빐 Comparable瑜� 援ы쁽�빀�땲�떎.
     static class Node implements Comparable<Node>{
-        int index;			// 노드 번호
-        int distacne;		// 이동 할 노드까지의 거리
+        int index;			// �끂�뱶 踰덊샇
+        int distacne;		// �씠�룞 �븷 �끂�뱶源뚯��쓽 嫄곕━
 
         Node(int index, int distacne) {
             this.index = index;
             this.distacne = distacne;
         }
 
-        // 최단거리를 기준으로 오름차순 정렬합니다.
+        // 理쒕떒嫄곕━瑜� 湲곗��쑝濡� �삤由꾩감�닚 �젙�젹�빀�땲�떎.
         @Override
         public int compareTo(Node o) {
             return Integer.compare(this.distacne, o.distacne);
