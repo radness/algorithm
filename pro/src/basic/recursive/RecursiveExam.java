54f4dd71bff408ccbe6aafb89a6ef456a862c814
radness <yhs5128@gmail.com>
2023-04-19 09:07:09 +0900

    재귀호출 - used 가지치기

--- pro/src/basic/recursive/RecursiveExam.java
+++ pro/src/basic/recursive/RecursiveExam.java
@@ -7,7 +7,13 @@ public class RecursiveExam {
 		run(0, 0);
 	}
 
+	// 기본 재귀호출
 	static void run(int num, int sum) {
+		// 재귀호출 -> 모든 경우를 다 시도 -> 모든 답을 구하기 때문에 느리다.
+		// 가지치기
+		if (sum > 10)
+			return;
+		
 		if (num == 3) {
 			for (int i = 0; i < num; i++) {
 				System.out.print(path[i]);
@@ -16,7 +22,7 @@ public class RecursiveExam {
 			return;
 		}
 
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 6; i++) {
 			path[num] = i + 1;
 
 			run(num + 1, sum + i);
---
+++ pro/src/basic/recursive/RecursiveExam2.java
@@ -0,0 +1,31 @@
+package basic.recursive;
+
+public class RecursiveExam2 {
+	static int[] path = new int[10];
+	static int[] used = new int[10];
+	
+	public static void main(String[] args) {
+		run(0);
+	}
+
+	// used 가지치기
+	// 한번 나온 눈금은 다시 나오지 않도록 하는것
+	static void run(int num) {
+		if (num == 3) {
+			for (int i = 0; i < num; i++) {
+				System.out.print(path[i]);
+			}
+			System.out.println();
+			return;
+		}
+
+		for (int i = 0; i < 3; i++) {
+			if (used[i] == 1) continue;
+			used[i] = 1;
+			path[num] = i + 1;
+			run(num + 1);
+			path[num] = 0;
+			used[i] = 0;
+		}
+	}
+}
