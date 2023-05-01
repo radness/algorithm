bbaa66af62e30194f32ff1e1f77e084fe0362706
radness <yhs5128@gmail.com>
2023-05-01 17:34:03 +0900

    경로 정리

--- pro/src/basic/list/LinkedListExam.java
+++ pro/src/basic/list/LinkedListExam.java
@@ -1,23 +1,39 @@
-package basic.list;
+package basic;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 
+// LinkedList
+// 데이터 + 다음 노드 정보 저장
 public class LinkedListExam {
 	public static void main(String[] args) throws Exception {
+		
 		LinkedList<Integer> list = new LinkedList<>();
-
+		
+		// 값 추가
 		list.add(1);
-		list.add(0, 10);
-
+		list.add(2);
+		list.add(3);
+		
 		System.out.println(list);
-
-		ArrayList<Integer> list2 = new ArrayList<>();
-
-		list2.add(1);
-		list2.add(0, 10);
-
-		System.out.println(list2);
+		
+		// 인덱스 1번에 데이터 10 추가
+		list.add(1, 10);
+		
+		// 값 삭제
+		list.remove(2);
+		System.out.println(list);
+		
+		// 맨 앞에 값 추가
+		list.addFirst(100);
+		System.out.println(list);
+		
+		// 맨 뒤에 값 추가
+		list.addLast(300);
+		System.out.println(list);
+		
+		// 값 확인
+		// 값이 존재하면 true, 없으면 false
+		System.out.println(list.contains(1));
 		
 	}
 }
