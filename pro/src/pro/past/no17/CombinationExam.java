1152d4c53a8a4d86eef0fe350c1812e955d49f7b
radness <yhs5128@gmail.com>
2023-06-14 16:02:41 +0900

    조합 nCr

---
+++ pro/src/pro/past/no17/CombinationExam.java
@@ -0,0 +1,51 @@
+package pro.past.no17;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class CombinationExam {
+	static List<Integer> arr;
+	static boolean[] visited = new boolean[5];
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+
+		arr = new ArrayList<>();
+
+		for (int i = 1; i <= N; i++) {
+			arr.add(i);
+		}
+
+		combination(arr, visited, 0, R);
+
+	}
+
+	private static void combination(List<Integer> arr, boolean[] visited, int index, int r) {
+		if (r == 0) {
+			for (int i = 0; i < arr.size(); i++) {
+				if (visited[i])
+					System.out.print(arr.get(i) + " ");
+			}
+			System.out.println();
+			return;
+		}
+
+		if (index == arr.size()) {
+			return;
+		}
+
+		visited[index] = true;	// 선택하는 경우
+		combination(arr, visited, index + 1, r - 1);
+
+		visited[index] = false;	// 선택하지 않는 경우
+		combination(arr, visited, index + 1, r);
+
+	}
+}
