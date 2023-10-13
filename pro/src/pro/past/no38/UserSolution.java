5c16965f8c973337f8f49c79007226c0140b52f4
radness <yhs5128@gmail.com>
2023-10-13 10:18:07 +0900

    물품보관소

--- pro/src/pro/past/no38/UserSolution.java
+++ pro/src/pro/past/no38/UserSolution.java
@@ -1,183 +1,121 @@
 package pro.past.no38;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.TreeMap;
-
 // 물품보관
+// 세그먼트 트리
 class UserSolution {
-	static int SIZE;
-	static int[] STORE;
-	// key : position
-	// value : height
-	static TreeMap<Integer, Integer> tmap;
-	static TreeMap<Integer, ArrayList<Integer>> tLocation;
-	static ArrayList<Integer> list;
-	static PriorityQueue<Node> pqMin;
-	static PriorityQueue<Node> pqMax;
-
-	static class Node implements Comparable<Node> {
-		int heigit;
-		int idx;
-
-		public Node(int heigit, int idx) {
-			this.heigit = heigit;
-			this.idx = idx;
-		}
-
-		@Override
-		public int compareTo(Node other) {
-			if (this.heigit > other.heigit)
-				return -1;
-			if (this.heigit < other.heigit)
-				return 1;
-			if (this.idx > other.idx)
-				return 1;
-			if (this.idx < other.idx)
-				return -1;
-			return 0;
-		}
-	}
+	int[] HEIGHT; // i번 위치의 높이
+	int[] TREE; // 해당 구간 중 물품의 높이가 가장 높은곳의 위치값 저장
+	int LEAF;
 
 	public void init(int N) {
-		SIZE = N;
-		STORE = new int[100001];
-		tmap = new TreeMap<>();
-		tLocation = new TreeMap<>();
+		LEAF = 1;
+
+		while (LEAF < N) {
+			LEAF <<= 1;
+		}
+
+		HEIGHT = new int[LEAF + 1];
+		TREE = new int[LEAF * 2];
+
+		for (int i = 0; i < LEAF; i++) {
+			TREE[LEAF + i] = i + 1;
+		}
+		for (int i = LEAF - 1; i > 0; i--) {
+			TREE[i] = TREE[i * 2];
+		}
 
 		return;
 	}
 
 	public int stock(int mLoc, int mBox) {
-		STORE[mLoc] += mBox;
-		if (tmap.get(mLoc) != null) {
-			int height = tmap.get(mLoc) + mBox;
-			tmap.put(mLoc, height);
-			ArrayList<Integer> delList = tLocation.get(mBox);
-			for (Integer item : delList) {
-				if (item == mLoc) {
-					delList.remove(item);
-					break;
-				}
-			}
-			ArrayList<Integer> addList = new ArrayList<>();
-			addList.add(mLoc);
-			tLocation.put(height, addList);
-		} else {
-			tmap.put(mLoc, mBox);
-			list = tLocation.get(mBox) == null ? new ArrayList<>() : tLocation.get(mBox);
-			list.add(mLoc);
-			tLocation.put(mBox, list);
-		}
-
+		HEIGHT[mLoc] += mBox;
+		rebuild(mLoc);
 		return getSize();
-	}
-
-	// 필요한 보호 천막의 크기 반환
-	private int getSize() {
-		int ans = 0;
-		int maxStart = 0;
-		int maxEnd = 0;
-		int maxHeight = 0;
-		int maxIdx = 1;
-
-		for (int i = 1; i < SIZE; i++) {
-			if (STORE[i] > maxHeight) {
-				maxHeight = STORE[i];
-				maxIdx = i;
-			}
-		}
-
-		ArrayList<Integer> list = tLocation.get(maxHeight);
-
-		if (list == null)
-			return 0;
-
-		Collections.sort(list);
-
-		if (list.size() == 1) {
-			maxStart = maxEnd = list.get(0);
-		} else {
-			maxStart = list.get(0);
-			maxEnd = list.get(list.size() - 1);
-		}
-
-		ans += (maxEnd - maxStart + 1) * maxHeight;
-
-		pqMin = new PriorityQueue<>();
-		pqMax = new PriorityQueue<>();
-
-		for (Map.Entry<Integer, Integer> entry : tmap.subMap(0, true, maxStart, false).entrySet()) {
-			pqMin.add(new Node(entry.getValue(), entry.getKey()));
-		}
-		for (Map.Entry<Integer, Integer> entry : tmap.subMap(maxEnd, false, SIZE, true).entrySet()) {
-			pqMax.add(new Node(entry.getValue(), entry.getKey()));
-		}
-
-		int leftSize = getLeft(maxStart, pqMin);
-		int rightSize = getRight(maxEnd, pqMax);
-
-		ans += leftSize;
-		ans += rightSize;
-
-		return ans;
-	}
-
-	private int getRight(int maxIdx, PriorityQueue<Node> pqRight) {
-		int rightSize = 0;
-		while (!pqRight.isEmpty()) {
-			Node now = pqRight.poll();
-			if (maxIdx > now.idx)
-				continue;
-			int width = now.idx - maxIdx;
-			maxIdx = now.idx;
-			rightSize += width * now.heigit;
-		}
-
-		return rightSize;
-	}
-
-	private int getLeft(int maxIdx, PriorityQueue<Node> pqLeft) {
-		int leftSize = 0;
-		while (!pqLeft.isEmpty()) {
-			Node now = pqLeft.poll();
-			if (maxIdx < now.idx)
-				continue;
-			int width = maxIdx - now.idx;
-			maxIdx = now.idx;
-			leftSize += width * now.heigit;
-		}
-
-		return leftSize;
 	}
 
 	public int ship(int mLoc, int mBox) {
-		STORE[mLoc] -= mBox;
-		int height = tmap.get(mLoc) - mBox;
-		if (height == 0) {
-			tmap.remove(mLoc);
-		} else {
-			tmap.put(mLoc, height);
-		}
-
-		ArrayList<Integer> list = tLocation.get(mBox);
-		for (Integer item : list) {
-			if (item == mLoc) {
-				list.remove(item);
-				break;
-			}
-		}
-		tLocation.remove(mBox);
-		if (!list.isEmpty()) {
-			tLocation.put(mBox, list);
-		}
-
+		HEIGHT[mLoc] -= mBox;
+		rebuild(mLoc);
 		return getSize();
 	}
 
-	public int getHeight(int mLoc) {
-		return tmap.get(mLoc) == null ? 0 : tmap.get(mLoc);
+	private void rebuild(int mLoc) {
+		int position = (LEAF + mLoc - 1) / 2;
+
+		while (position > 0) {
+			TREE[position] = HEIGHT[TREE[position * 2]] >= HEIGHT[TREE[position * 2 + 1]] ? TREE[position]
+					: TREE[position * 2 + 1];
+			position >>= 1;
+		}
+
 	}
+
+	// 천막의 크기를 구한다.
+	private int getSize() {
+		// 전체 구간에서 가장 높은곳의 위치 확인 = top
+		int size = HEIGHT[TREE[1]];
+		// 1 ~ (top - 1) 의 넓이
+		int end = TREE[1] - 1;
+		// 왼쪽 천막의 넓이
+		while (end > 0) {
+			// 탐색할 구간 중 가장 높이가 높은 곳의 위치를 확인
+			int topIdx = getMax(1, 1, LEAF, 1, end);
+
+			// 물품이 없으면 탐색 x
+			if (HEIGHT[topIdx] == 0) {
+				break;
+			}
+			// 가로길이 * 높이
+			size += (end - topIdx + 1) * HEIGHT[topIdx];
+			end = topIdx - 1; // 마지막 위치의 앞칸
+		}
+		// (top + 1) ~ LEAF(N) 의 넓이 (오른쪽 천막의 넓이)
+		int start = TREE[1] + 1;
+		while (start <= LEAF) {
+			int topIdx = getMax(1, 1, LEAF, start, LEAF); // 가장 높은 곳
+
+			if (HEIGHT[topIdx] == 0) {
+				break;
+			}
+			// 크기는 = 가로 * 높이
+			size += (topIdx - start + 1) * HEIGHT[topIdx];
+			start = topIdx + 1;
+		}
+
+		return size;
+	}
+
+	// 세그먼트 트리
+	// start ~ end 구간 중 물품이 가장 높에 쌓여있는 곳의 위치
+	private int getMax(int pos, int rangeStart, int rangeEnd, int start, int end) {
+		// 찾고싶은 구간을 현재 위치에서 확인할 수 있는 경우
+		if (rangeStart == start && rangeEnd == end) {
+			return TREE[pos];
+		}
+
+		// 내가 찾고자 하는 영역이
+		int mid = (rangeStart + rangeEnd) / 2;
+
+		// 1. start ~ end 가 모두 왼쪽
+		if (end <= mid) {
+			return getMax(pos * 2, rangeStart, mid, start, end);
+		}
+		// 2. start ~ end 가 모두 오른쪽
+		if (start > mid) {
+			return getMax(pos * 2 + 1, mid + 1, rangeEnd, start, end);
+		}
+
+		// 3. 양쪽에 걸쳐있는 경우
+		int leftIdx = getMax(pos * 2, rangeStart, mid, start, end);
+		int rightIdx = getMax(pos * 2 + 1, mid + 1, rangeEnd, mid + 1, end);
+
+		// 구간의 높이 값으로 비교
+		return HEIGHT[leftIdx] >= HEIGHT[rightIdx] ? leftIdx : rightIdx;
+	}
+
+	// mLoc 위치의 높이를 구한다.
+	public int getHeight(int mLoc) {
+		return HEIGHT[mLoc];
+	}
+
 }
